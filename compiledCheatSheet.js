58618776b5181c27e27b5ebbea59ad7779a5b4ac
Source Academy User <No public email provided>
2023-11-02 14:07:52 +0800

    Changes made from Source Academy

--- compiledCheatSheet.js
+++ compiledCheatSheet.js
@@ -1,7 +1,10 @@
 function permutation(xs){
-    return accumulate((x,y)=>append(x,y),null,
-    map(term1=>map(term2=>pair(term1,term2),remove(xs,term2)),xs));
+    return is_null(xs)
+        ? list(null)
+        : accumulate((x,y)=>append(x,y)
+            ,null,map(term1=>map(term2=>pair(display(term1),term2),
+            permutation(remove(term1,xs))),xs));
 }
-
+//base case must be correct for permutation to work well! 
 permutation(list(1,2,3));
     
