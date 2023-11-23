4d0887845af17b8e60e6e1e25d9859e3bd98a890
Source Academy User <No public email provided>
2023-11-23 11:51:09 +0800

    Changes made from Source Academy

--- FrappucinoFinals.js
+++ FrappucinoFinals.js
@@ -1,1 +1,11 @@
 //2020/2021 Final assessment sem1
+function accumulate_iter(f, init, xs){
+    function iter_helper(f,init,xs_new){
+        return is_null(xs_new)
+            ? init
+            : iter_helper(f,display(f(display(head(xs_new),"head"),init)),tail(xs_new));
+    }
+    return iter_helper(f,init,reverse(xs));
+}
+
+accumulate_iter((x,y)=>x/y,2, list(24,16,8));
