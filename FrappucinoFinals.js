155ab59dcccce42883b073bfa6ae926e23655ac9
Source Academy User <No public email provided>
2023-11-23 11:53:52 +0800

    Changes made from Source Academy

--- FrappucinoFinals.js
+++ FrappucinoFinals.js
@@ -9,3 +9,16 @@ function accumulate_iter(f, init, xs){
 }
 
 accumulate_iter((x,y)=>x/y,2, list(24,16,8));
+
+//This method uses continuous passing style, which basically forms a long long lambda function. 
+// Over here, you need a helper function to keep operating on the lambda expression. 
+function accumulate_iterate(f,init,xs){
+    function acc(ys,cont){
+        return is_null(ys)
+            ? cont(init)
+            : acc(tail(ys),x=>cont(f(head(ys),x)));
+    }
+    return acc(xs,x=>x);
+}
+
+accumulate_iterate((x,y)=>x/y,2, list(24,16,8));
